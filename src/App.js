e1bae2b1e04d3de166a6098fe62926b8fc17e6f0
Marju Tubli <marjutubli@gmail.com>
2021-11-20 13:55:45 +0200

    Complete session

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import pokemonData from './pokemon-data';
+import { useState, useEffect } from 'react';
 import PokemonList from './PokemonList'
 import Pokemon from './Pokemon'
+import pokemons from './pokemon-data';
+
 const initialPokemon = {
   name: 'pikachu',
   url: 'https://pokeapi.co/api/v2/pokemon/25'
@@ -9,15 +10,45 @@ const initialPokemon = {
 
 function App() {
   const [currentPokemon, setCurrentPokemon] = useState(initialPokemon)
+  const [pokemonData, setPokemonData] = useState([])
+
+  useEffect(() => {
+    const pokemon = localStorage.getItem("pokemon")
+
+    if (pokemon) {
+      setCurrentPokemon(JSON.parse(pokemon))
+    }
+
+    async function getData() {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+        const data = await response.json()
+
+        setPokemonData(data.results)
+      } catch {
+        setPokemonData(pokemons.results)
+      }
+    }
+
+    getData()
+  }, [])
+
+  useEffect(() => {
+    document.title = currentPokemon.name
+    if (currentPokemon) {
+      localStorage.setItem("pokemon", JSON.stringify(currentPokemon))
+    }
+  }, [currentPokemon])
 
   return (
     <div className="App">
       <button onClick={() => setCurrentPokemon("")}>
         Clear Pokemon
       </button>
+
       <div className="container">
         <PokemonList
-          pokemonData={pokemonData.results}
+          pokemonData={pokemonData}
           setCurrentPokemon={setCurrentPokemon}
         />
         {currentPokemon && <Pokemon pokemon={currentPokemon} />}
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react"
+
+const initialState = { name: "", height: "", weight: "", sprites: { front_default: "" } }
+
 function Pokemon(props) {
+  const [pokemon, setPokemon] = useState(initialState)
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(props.pokemon.url)
+      const data = await response.json()
+      setPokemon(data)
+    }
+
+    getData()
+  }, [props.pokemon])
+
+  useEffect(() => {
+    return () => {
+      localStorage.removeItem("pokemon")
+    }
+  }, [])
 
   return (
     <div className="pokemon">
-      <img alt="" src="" />
-      <h2>{props.pokemon.name}</h2>
-      <h2>height:</h2>
-      <h2>weight:</h2>
+      <img alt="" src={pokemon.sprites.front_default} />
+      <h2>{pokemon.name}</h2>
+      <h2>height: {pokemon.height}</h2>
+      <h2>weight: {pokemon.weight}</h2>
     </div>
   )
 }
